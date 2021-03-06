db64731c4347fb78ba339990321b6beb8c374748
victor <victor8titov@yandex.ru>
2021-03-06 20:10:46 +0300

    test

--- settings/settings-projects-page.php
+++ settings/settings-projects-page.php
@@ -1,14 +1,17 @@
-<?php 
-add_action( 'wp_print_scripts', 'deregister_javascript_nextgen_gallery_plugin', 100 );
+<?php
+add_action('wp_print_scripts', 'deregister_javascript_nextgen_gallery_plugin', 100);
 
-function deregister_javascript_nextgen_gallery_plugin() 
- { 
-    if ( is_page_template('projects.php') ) 
-      {
-        wp_deregister_style('fancybox-0');
-        wp_dequeue_style('fancybox-0');
-        wp_deregister_script('ngg_lightbox_context');
-      } 
- } 
+function deregister_javascript_nextgen_gallery_plugin()
+{
+  if (is_page_template('projects.php')) {
+    wp_deregister_style('fancybox-0');
+    wp_dequeue_style('fancybox-0');
+    wp_deregister_script('ngg_lightbox_context');
+  }
+}
 
- 
+add_action('wp_enqueue_scripts', function () {
+  if (is_page_template('projects.php')) {
+    wp_dequeue_style('fancybox-0');
+  }
+});
