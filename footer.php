2dce21014a77c5519743ce940648ec0c172efa9e
victor <victor8titov@yandex.ru>
2021-03-06 20:16:11 +0300

    remove dev script

--- footer.php
+++ footer.php
@@ -79,10 +79,6 @@
 
 <?php wp_footer(); ?>
 
-<script id="__bs_script__">//<![CDATA[
-    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.26.7'><\/script>".replace("HOST", location.hostname));
-//]]></script>
-
 <script>
 var acc = document.getElementsByClassName("accordion");
 var i;
