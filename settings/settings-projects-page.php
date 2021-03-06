6738d662dd98bf821d19c8e09131e29190eb2d56
victor <victor8titov@yandex.ru>
2021-03-06 19:17:59 +0300

    test

--- settings/settings-projects-page.php
+++ settings/settings-projects-page.php
@@ -5,7 +5,8 @@ function deregister_javascript_nextgen_gallery_plugin()
  { 
     if ( is_page_template('projects.php') ) 
       {
-        //wp_deregister_script('fancybox'); 
+        wp_deregister_script('fancybox'); 
+        wp_deregister_script('ngg_lightbox_context');
       } 
  } 
 
