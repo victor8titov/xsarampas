ff913a8d7603b0f4f81407fb1944849ae8fb73db
victor <victor8titov@yandex.ru>
2021-03-06 13:37:59 +0300

    adde comment

--- template-parts/parts-main-page/show_video.php
+++ template-parts/parts-main-page/show_video.php
@@ -3,6 +3,7 @@ $show_video = new UtilsMetaBox(SHOW_VIDEO);
 ?>
 
 <?php if ($show_video->check_field('is_show') && $show_video->check_field('iframe')) : ?>
+	<!-- video block -->
 	<div class="row">
 		<div class="video">
 			<iframe width="100%" height="455" src="<?php $show_video->put_text('url'); ?>" frameborder="0" allowfullscreen />
